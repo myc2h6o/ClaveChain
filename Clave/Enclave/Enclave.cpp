154524f4b2d97819c420df60ee294a68d2649cfa
mycspring <1248036495@qq.com>
2017-03-21 21:45:30 +0800

    Add print function

--- Clave/Enclave/Enclave.cpp
+++ Clave/Enclave/Enclave.cpp
@@ -1,3 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Enclave.h"
 #include "Enclave_t.h"
 
-#include "sgx_trts.h"
+/*
+ * oprintf:
+ * Helper function to print to terminal outside enclave
+ */
+void oprintf(const char *fmt, ...) {
+    char buf[BUFSIZ] = { '\0' };
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_printString(buf);
+}
+
+/*
+ * ecall_printHello:
+ * Test function for oprintf
+ */
+void ecall_printHello(void) {
+    char *str = "Hello From Enclave";
+    int number = 1;
+    oprintf("%s %d!\n", str, number);
+}
---
+++ Clave/Enclave/Enclave.h
@@ -0,0 +1,14 @@
+#ifndef _ENCLAVE_H
+#define _ENCLAVE_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+    void oprintf(const char *fmt, ...);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
