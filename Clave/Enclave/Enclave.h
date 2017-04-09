fdaa76ec3fab34a57918cd7e62c5eb7e585835e3
mycspring <1248036495@qq.com>
2017-04-09 10:39:22 +0800

    Move output to new file

--- Clave/Enclave/Enclave.h
+++ Clave/Enclave/Enclave.h
@@ -1,14 +1,4 @@
 #ifndef _ENCLAVE_H
 #define _ENCLAVE_H
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-    void oprintf(const char *fmt, ...);
-
-#if defined(__cplusplus)
-}
-#endif
-
 #endif
---
+++ Clave/Enclave/Output.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Output.h"
+#include "Enclave_t.h"
+
+/*
+* oprintf:
+* Helper function to print to terminal outside enclave
+*/
+void oprintf(const char *fmt, ...) {
+    char buf[BUFSIZ] = { '\0' };
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_printString(buf);
+}
+
+/*
+* ecall_printHello:
+* Test function for oprintf
+*/
+void ecall_printHello(void) {
+    oprintf("%s %d!\n", "Hello From Enclave", 1);
+}
