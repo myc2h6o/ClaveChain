e0f944717c51ce9b6da24cd9debe28b0553458d8
mycspring <1248036495@qq.com>
2017-04-15 11:14:05 +0800

    Add mbedtls ecsda key usage.

--- Clave/Enclave/Secret.cpp
+++ Clave/Enclave/Secret.cpp
@@ -4,37 +4,79 @@
 #include "Enclave_t.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
-#include "mbedtls/bignum.h"
-#include "mbedtls/x509.h"
-#include "mbedtls/rsa.h"
+#include "mbedtls/ecdsa.h"
 
-#define KEY_SIZE 2048
-#define EXPONENT 65537
+void dump_buf(const char *title, unsigned char *buf, size_t len) {
+    size_t i;
+    unsigned char outbuf[129];
+    oprintf("%s:\n", title);
+    for (i = 0; i < len; i++) {
+        outbuf[2 * i] = "0123456789ABCDEF"[buf[i] / 16];
+        outbuf[2 * i + 1] = "0123456789ABCDEF"[buf[i] % 16];
+    }
+    outbuf[2 * i] = 0;
+    oprintf("%s\n", outbuf);
+}
+
+void dump_address(const char *title, mbedtls_ecdsa_context *key) {
+    unsigned char buf[65];
+    size_t len;
+    if (mbedtls_ecp_point_write_binary(&key->grp, &key->Q, MBEDTLS_ECP_PF_UNCOMPRESSED, &len, buf, sizeof buf) != 0) {
+        oprintf("internal error\n");
+        return;
+    }
+    //[TODO] Add sha3
+}
+
+void dump_pubkey(const char *title, mbedtls_ecdsa_context *key)
+{
+    unsigned char buf[65];
+    size_t len;
+    if (mbedtls_ecp_point_write_binary(&key->grp, &key->Q, MBEDTLS_ECP_PF_UNCOMPRESSED, &len, buf, sizeof buf) != 0) {
+        oprintf("internal error\n");
+        return;
+    }
+
+    dump_buf(title, buf, len-1);
+}
+
+void dump_prikey(const char *title, mbedtls_mpi *key) {
+    unsigned char buf[33];
+    size_t len = 0;
+    mbedtls_mpi_write_binary(key, buf, 33);
+    dump_buf(title, buf, 32);
+}
 
 void ecall_generateKeyPair() {
     int ret;
-    mbedtls_rsa_context rsa;
+    mbedtls_ecdsa_context context;
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_entropy_init(&entropy);
-    const char *pers = "rsa_genkey";
+    const char *pers = "ecdsa";
 
-    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-        (const unsigned char *)pers,
-        strlen(pers))) != 0)
-    {
+    mbedtls_ecdsa_init(&context);       //init ecsda context
+    mbedtls_ctr_drbg_init(&ctr_drbg);   //set entropy source
+    mbedtls_entropy_init(&entropy);     //random seed
+
+    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char *)pers, strlen(pers))) != 0) {
         oprintf(" failed\n  ! mbedtls_ctr_drbg_seed returned %d\n", ret);
         return;
     }
-    oprintf(" ok\n  . Generating the RSA key [ %d-bit ]...\n", KEY_SIZE);
-    mbedtls_rsa_init(&rsa, MBEDTLS_RSA_PKCS_V15, 0);
-    if ((ret = mbedtls_rsa_gen_key(&rsa, mbedtls_ctr_drbg_random, &ctr_drbg, KEY_SIZE,
-        EXPONENT)) != 0)
-    {
-        oprintf(" failed\n  ! mbedtls_rsa_gen_key returned %d\n", ret);
+
+    if ((ret = mbedtls_ecdsa_genkey(&context, MBEDTLS_ECP_DP_SECP256K1, mbedtls_ctr_drbg_random, &ctr_drbg)) != 0) {
+        oprintf(" failed\n  ! mbedtls_ecdsa_genkey returned %d\n", ret);
         return;
     }
-    oprintf("rsa.d: %d\n", rsa.D.p);
+
+    oprintf(" ok (key size: %d bits)\n", (int)context.grp.pbits);
+    dump_pubkey("Public key: ", &context);
+    dump_prikey("Secret Key: ", &context.d);
+    // Note: address use hash of binary
+    dump_address("Address: ", &context);
+
+    //clean context
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_entropy_free(&entropy);
 }
 
 void generateKeyPair(char** privateKey, char** publicKey) {
